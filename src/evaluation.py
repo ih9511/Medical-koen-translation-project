b7aa87e5774c53cad7f335cbcdb27f4f2e2e82b1
inheon <cih956964@gmail.com>
2025-02-22 15:25:04 +0000

    modify: evaluation, inference 로직 수정

--- src/evaluation.py
+++ src/evaluation.py
@@ -8,47 +8,44 @@ evaluation.py
 import pandas as pd
 import evaluate
 import logging
-import re
-
-from inference import load_model, translate_text
         
 
-def evaluate_translation(model, tokenizer, dataset_path: str):
-    """
-    번역 모델의 성능을 BLEU 점수를 통해 평가하는 함수 (Hugging Face evaluate 사용)
+# def evaluate_translation(dataset_path: str):
+#     """
+#     번역 모델의 성능을 BLEU 점수를 통해 평가하는 함수 (Hugging Face evaluate 사용)
     
-    :parameter model: 로드된 번역 모델
-    :parameter tokenizer: 해당 모델의 토크나이저
-    :parameter dataset_path: 평가에 사용할 CSV 데이터셋 경로
-    """
+#     :parameter dataset_path: 평가에 사용할 데이터 파일 경로
+#     :return: BLEU 점수
+#     """
     
-    df = pd.read_csv(dataset_path)
-    logging.warning('Inferencing...')
-    references = df['output'].tolist()
-    hypotheses = [translate_text(model, tokenizer, './data/processed_data/test_processed.csv') for text in df['input'].tolist()]
-    logging.warning('Inference complete!')
+#     df = pd.read_csv(dataset_path)
+#     references = df['output'].tolist()
+#     hypotheses = df['translated'].tolist()
+#     logging.warning('Data loading complete!')
     
-    for i in range(10):
-        print(f'hypothese: {hypotheses[i]}')
-        print(f'references: {references[i]}')
-        print('\n')
+#     for i in range(10):
+#         print(f'hypothese: {hypotheses[i]}')
+#         print(f'references: {references[i]}')
+#         print('\n')
     
-    # BLEU 평가
-    logging.warning('BLEU evaluation start')
-    bleu_metric = evaluate.load('bleu')
-    bleu_score = bleu_metric.compute(predictions=hypotheses, references=[[ref] for ref in references])
+#     # BLEU 평가
+#     logging.warning('BLEU evaluation start')
+#     bleu_metric = evaluate.load('bleu')
+#     bleu_score = bleu_metric.compute(predictions=hypotheses, references=[[ref] for ref in references])
     
-    logging.warning('Evaluation Complete!')
-    logging.warning(f"BLEU: {bleu_score['bleu']}")
+#     logging.warning('Evaluation Complete!')
+#     logging.warning(f"BLEU: {bleu_score}")
     
-    return {"BLEU": bleu_score['bleu']}
+#     return {"BLEU": bleu_score['bleu']}
 
+meteor = evaluate.load('meteor')
+df = pd.read_csv('./data/processed_data/llama_translated_original_model.csv')
+predictions = df['translated'].tolist()
+references = df['output'].tolist()
 
-if __name__ == '__main__':
-    base_model_name = 'MLP-KTLim/llama-3-Korean-Bllossom-8B'
-    finetune_model_name = 'ih9511/llama-3-Korean-8B_koen_medical_translation'
-    dataset_path = './data/processed_data/test_processed.csv'
-    
-    model, tokenizer = load_model(base_model_name, finetune_model_name)
-    # evaluation_results = evaluate_translation(model, tokenizer, dataset_path)
-    translate_text(model, tokenizer, dataset_path)
+results = meteor.compute(predictions=predictions, references=references)
+print(results)
+
+# if __name__ == '__main__':
+#     dataset_path = './data/processed_data/llama_translated.csv'
+#     evaluate_translation(dataset_path)
--- src/inference.py
+++ src/inference.py
@@ -1,3 +1,12 @@
+"""
+inference.py
+
+이 모듈은 파인튜닝된 모델을 통해 테스트 데이터에 대한 번역을 수행합니다.
+- Fine-tuned llama-3-Korean-Bllossom-8B 모델을 로드합니다.
+- 원본 모델(llama-3-Korean-Bllossom-8B)의 토크나이저를 로드합니다.
+- 테스트 데이터에 대한 번역을 수행하고 번역 데이터를 저장합니다.
+"""
+
 import pandas as pd
 import logging
 import re
@@ -7,34 +16,13 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 from typing import Tuple
 
 
-# def load_model(base_model_name: str, finetune_model_name: str) -> Tuple[AutoModelForCausalLM, AutoTokenizer]:
-#     """
-#     Hugging Face Hub에서 사전학습된 번역 모델을 로드합니다.
-    
-#     :parameter base_model_name: 토크나이저 세팅을 위한 원본 모델 저장소 경로
-#     :parameter finetune_model_name: 불러올 모델의 HuggingFace 저장소 경로
-#     :return: 로드된 모델과 토크나이저
-#     """
-#     model = AutoModelForCausalLM.from_pretrained(finetune_model_name, device_map='cuda')
-#     model.eval()
-#     tokenizer = AutoTokenizer.from_pretrained(base_model_name)
-#     logging.warning('Model and tokenizer load complete!')
-    
-#     # 추론 최적화
-#     model.generation_config.cache_implementation = 'static'
-#     model.forward = torch.compile(model.forward, mode='reduce-overhead', fullgraph=True)
-    
-#     return model, tokenizer
-
 def translate_text(model_id, tokenizer_id, data_dir: str):
     """
     입력 텍스트를 번역하는 함수
     
-    :parameter model: 로드된 번역 모델
-    :parameter tokenizer: 해당 모델의 토크나이저
-    :parameter text: 번역할 입력 문장
-    :parameter max_length: 번역된 문장의 최대 길이
-    :return: 번역된 텍스트
+    :parameter model_id: 로드된 번역 모델
+    :parameter tokenizer_id: 해당 모델의 토크나이저
+    :parameter data_dir: 번역된 데이터를 저장할 주소
     """
     
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_id)
@@ -43,6 +31,7 @@ def translate_text(model_id, tokenizer_id, data_dir: str):
         torch_dtype=torch.bfloat16,
         device_map='auto',
     )
+    
     # 추론 최적화
     model.generation_config.cache_implementation = 'static'
     model.forward = torch.compile(model.forward, mode='reduce-overhead', fullgraph=True)
@@ -58,10 +47,11 @@ def translate_text(model_id, tokenizer_id, data_dir: str):
         query = row['input']
         prompt = (
             '''
-            [[MEDICAL TRANSLATION MODE]] You are a helpful AI translator. Translate this English clinical sentence to Korean using:
-            - Korean sentence output MUST be only sentence. Don't add any other information that does not contained in original Korean sentence.
-            - Exact measurement units (preserve original numbers)
-            - No idiomatic adaptation
+            [[MEDICAL TRANSLATION MODE]] You are a highly accurate AI translator. Translate the following English clinical sentence into Korean with strict adherence to the original meaning and structure:
+            - Output must be a single, standalone Korean sentence. Do not include any additional information.
+            - Preserve exact measurement units (keep original numbers, units, and medical notations unchanged).
+            - Ensure precise, formal translation without idiomatic adaptions.
+            - Mainatain clinical terminology accuracy.
             '''
             f"English: {query}"
         )
@@ -95,17 +85,6 @@ def translate_text(model_id, tokenizer_id, data_dir: str):
             eos_token_id=terminators,
         )
         translated_text = tokenizer.decode(translated[0][input_ids.shape[-1]:], skip_special_tokens=True)
-        # translated_text = translated[0]['translation_text']
-        
-        # 출력 데이터로부터 한국어 문장만 추출
-        # output_start = translated_text.find('Korean:')
-        # if output_start != -1:
-        #     extracted_text = translated_text[output_start + len('Korean:'):].strip()
-        # else:
-        #     extracted_text = translated_text.strip()
-        
-        # extracted_text = re.sub(r'\[\[.*?\]\]', '', extracted_text) # [[END OF MEDICAL TRANSLATION MODE]] 제거
-        # extracted_text = re.sub(r'[\d\.\s]+$', '', extracted_text) # 문장 끝의 연속된 숫자 제거
         print(f"✅{i}번째 번역: {translated_text}\n")
         
         translated_text_list.append(translated_text.strip())
@@ -114,14 +93,16 @@ def translate_text(model_id, tokenizer_id, data_dir: str):
     test_df['translated'] = translated_text_list
     
     # 번역 결과 저장
-    output_path = '.data/processed_data/llama_translated.csv'
+    output_path = './data/processed_data/llama_translated_original_model.csv'
     test_df.to_csv(output_path, index=False)
     logging.warning('Translation result saved')
     
 
 if __name__ == '__main__':
+    # base_model_name = 'ih9511/llama-3-Korean-8B_koen_medical_translation'
     base_model_name = 'MLP-KTLim/llama-3-Korean-Bllossom-8B'
-    finetune_model_name = 'ih9511/llama-3-Korean-8B_koen_medical_translation'
+    # finetune_model_name = 'ih9511/llama-3-Korean-8B_koen_medical_translation'
+    finetune_model_name = 'MLP-KTLim/llama-3-Korean-Bllossom-8B'
     dataset_path = './data/processed_data/test_processed.csv'
     
     translate_text(model_id=finetune_model_name, tokenizer_id=base_model_name, data_dir=dataset_path)
